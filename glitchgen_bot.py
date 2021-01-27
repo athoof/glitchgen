7ca848f04c0aa023ccca944c9ec5e5ad55453e54
Athoof Riyaz <athoof@gmail.com>
2021-01-27 21:11:38 +0500

    Added glitchText

--- glitchgen_bot.py
+++ glitchgen_bot.py
@@ -5,6 +5,7 @@ from telegram.ext import MessageHandler, Filters
 import random
 import json
 import os
+from zalgo_text import zalgo   
 
 from dotenv import load_dotenv
 load_dotenv()
@@ -17,3 +18,18 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 
 dispatcher = updater.dispatcher
 
+def glitchText(update, context):
+  g = zalgo.zalgo().zalgofy(update.message.text)
+  context.bot.send_message(chat_id=update.effective_chat.id, text=g)
+  
+# def glitchImage(update, context):
+  
+def start(update, context): 
+  context.bot.send_message(chat_id=update.effective_chat.id, text="Send image or text to glitch")
+  
+start_handler = CommandHandler('start', start)
+dispatcher.add_handler(start_handler)
+
+glitchText_handler = MessageHandler(Filters.text & (~Filters.command), glitchText)
+dispatcher.add_handler(glitchText_handler)
+ 
