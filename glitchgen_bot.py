4741f64e3750a4394ba6db0e92a1d58013233108
Athoof Riyaz <athoof@gmail.com>
2021-01-27 15:50:48 +0100

    Init

---
+++ glitchgen_bot.py
@@ -0,0 +1,7 @@
+from telegram.ext import Updater
+updater = Updater(token='1424419734:AAHa8exx1shl6mKkc9gEYJU0y4i_T5glwrQ', use_context=True)
+
+import logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                     level=logging.INFO)
+
